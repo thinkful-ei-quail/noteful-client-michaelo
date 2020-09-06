695c4a989629656ad0678d449e2932f3ba5bca39
Michael Oldacre <mloldacre@live.com>
2020-09-06 17:51:35 -0400

    removed / from API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: `https://sheltered-beyond-73177.herokuapp.com/`,
+  API_ENDPOINT: `https://sheltered-beyond-73177.herokuapp.com`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
 
